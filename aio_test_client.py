eb43dd5bfa08b6041d43f6097479cf24202bdffc
Ross Reedstrom <ross@reedstrom.org>
2025-06-23 12:59:55 -0500

    cleaner pretty print

--- aio_test_client.py
+++ aio_test_client.py
@@ -3,6 +3,7 @@ import argparse
 import asyncio
 import socket
 import pprint
+import dataclasses
 
 async def main(hostname):
     """Connect to an ESPHome device and get details."""
@@ -11,21 +12,34 @@ async def main(hostname):
     api = aioesphomeapi.APIClient(hostname, 6053, "")
     await api.connect(login=True)
 
-    pp = pprint.PrettyPrinter(indent=2, width=120)
+    pp = pprint.PrettyPrinter(indent=2, width=100)
+
     # Get API version of the device's firmware
-    pp.pprint(api.api_version)
+    print("\n=== API Version ===")
+    print(api.api_version)
 
     # Show device details
+    print("\n=== Device Info ===")
     device_info = await api.device_info()
     pp.pprint(device_info)
 
-    # List all entities of the device
-    entities = await api.list_entities_services()
-    pp.pprint(entities)
+    # List all entities and services of the device
+    entities, services = await api.list_entities_services()
+
+    print("\n=== Entities ===")
+    for entity in entities:
+        cls_name = type(entity).__name__
+        print(f"\n--- Entity: {cls_name} ---")
+        pp.pprint(entity)
+
+    print("\n=== Services ===")
+    for service in services:
+        cls_name = type(service).__name__
+        print(f"\n--- Service: {cls_name} ---")
+        pp.pprint(service)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ESPHome client tester")
-    parser.add_argument("--hostname", default=socket.gethostname(), help="Hostname or IP to connect to")
+    parser.add_argument("hostname", help="Hostname or IP to connect to")
     args = parser.parse_args()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(args.hostname))
+    asyncio.run(main(args.hostname))
